49dbf2ff15b5ec523bac2ffee80a39817948b086
Chris Bian <chris.a.bianchet@gmail.com>
2017-01-21 20:58:27 -0500

    Fixed wills merge

--- app/src/main/java/com/example/chris/santashelper/RestService.java
+++ app/src/main/java/com/example/chris/santashelper/RestService.java
@@ -5,13 +5,6 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 
-import com.mashape.unirest.http.Headers;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.async.Callback;
-import com.mashape.unirest.http.exceptions.UnirestException;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStream;
@@ -45,29 +38,6 @@ public class RestService extends Service {
                                    HashMap<String, String> postDataParams) {
 
 
-        Future<HttpResponse<JsonNode>> future = Unirest.post("http://ec2-54-186-119-141.us-west-2.compute.amazonaws.com:8080/generateSession")
-                .header("accept", "application/json")
-                .field("param1", "value1")
-                .asJsonAsync(new Callback<JsonNode>() {
-
-                    public void failed(UnirestException e) {
-                        System.out.println("The request has failed");
-                    }
-
-                    public void completed(HttpResponse<JsonNode> response) {
-                        int code = response.getStatus();
-                        Headers headers = response.getHeaders();
-                        JsonNode body = response.getBody();
-                        InputStream rawBody = response.getRawBody();
-                    }
-
-                    public void cancelled() {
-                        System.out.println("The request has been cancelled");
-                    }
-
-                });
-
-        return future.toString();
     }
 
     private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
