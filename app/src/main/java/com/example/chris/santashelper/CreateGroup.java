ffed650730e7f6650a59841bedabe9157e8df23e
Chris Bian <chris.a.bianchet@gmail.com>
2017-01-22 02:08:50 -0500

    added android main

--- app/src/main/java/com/example/chris/santashelper/CreateGroup.java
+++ app/src/main/java/com/example/chris/santashelper/CreateGroup.java
@@ -3,6 +3,10 @@ package com.example.chris.santashelper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -10,7 +14,7 @@ import org.w3c.dom.Text;
 public class CreateGroup extends AppCompatActivity {
 
     TextView usernameText;
-
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +23,7 @@ public class CreateGroup extends AppCompatActivity {
         usernameText = (TextView) findViewById(R.id.usernameText);
         Intent loginIntent = getIntent();
         usernameText.setText("Welcome, "+loginIntent.getStringExtra("username"));
-
-  //      RestManager restClient = new RestManager("wat");
-  //      restClient.execute("e");
+        list = (ListView) findViewById(R.id.listOfSession);
+        ListAdapter listAdapter = new ArrayAdapter<String>();
     }
 }
